3daa2d9be63d2909b1a6628ced8f1c5bfaafbf6f
PjBierhals <pjbierhals@gmail.com>
2024-08-11 16:26:05 -0300

    objetos

--- iniciando-com-typescript/src/index.ts
+++ iniciando-com-typescript/src/index.ts
@@ -2,4 +2,5 @@
 // import './tipos/enums'
 // import './tipos/enums_const'
 // import './tipos/enums_tipo'
-import './tipos/funcoes'
+// import './tipos/funcoes'
+import './tipos/objetos'
---
+++ iniciando-com-typescript/src/tipos/objetos.ts
@@ -0,0 +1,13 @@
+type Usuario= {
+nome:string,
+email:string
+}
+
+
+let usuario:Usuario ={
+    nome: 'joao',
+    email: 'joao@gmail.com'
+}
+
+console.log(usuario.nome)
+console.log(usuario.email)
