f44690fa1b0f9ea614266d26be7a9cf6b9e44c8a
PjBierhals <pjbierhals@gmail.com>
2024-08-29 13:17:29 -0300

    Tipos de Interfaces

--- iniciando-com-typescript/src/interfaces/usuario.ts
+++ iniciando-com-typescript/src/interfaces/usuario.ts
@@ -1,18 +1,48 @@
 interface Usuario{
-    id: number
+    id?: number
     nome: string
     email: string
+    senha?: String
 }
 
 
 const usuarioLogado: Usuario = {
     id: 123,
     nome: 'João',
-    email:' joao@email.com'
+    email:'joao@email.com',
+    senha:'12345'
 }
 
+console.log('Usuário')
 console.log(usuarioLogado.id)
 console.log(usuarioLogado.nome)
 console.log(usuarioLogado.email)
+console.log(usuarioLogado.senha)
 
+const usuarioOpcional: Partial<Usuario> = {
+    id:4321,
+    nome:'Jaime',
+    senha:'1234567y'
+}
+
+console.log('')
+console.log('Usuário Partial')
+console.log(usuarioOpcional.id)
+console.log(usuarioOpcional.nome)
+console.log(usuarioOpcional.email)
+console.log(usuarioOpcional.senha)
+
+const usuarioCompleto: Required<Usuario> = {
+    id:4321,
+    nome:'Ana',
+    email: 'ana@email.com',
+    senha:'1234567y'
+}
+
+console.log(' ')
+console.log('Usuário Required')
+console.log(usuarioCompleto.id)
+console.log(usuarioCompleto.nome)
+console.log(usuarioCompleto.email)
+console.log(usuarioCompleto.senha)
 export{}
